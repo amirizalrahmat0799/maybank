b53a7dde9c2d0fa8145d5bb296d87f195652eb30
Amirizal <amirizalrahmat@penril.net>
2024-11-04 07:55:16 +0800

    Initial commit

---
+++ src/main/java/com/mbb/maybank_spring_app/MaybankSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.mbb.maybank_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaybankSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaybankSpringAppApplication.class, args);
+	}
+
+}
