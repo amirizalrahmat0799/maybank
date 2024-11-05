784a503f4b81bc2d33e51a4f8e3e50771208b623
Amirizal <amirizalrahmat@penril.net>
2024-11-05 17:12:53 +0800

    Add Exception handler class

---
+++ src/main/java/my/com/mbb/controller/ControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package my.com.mbb.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice(basePackages = "my.com.mbb.controller")
+public class ControllerExceptionHandler {
+
+}
