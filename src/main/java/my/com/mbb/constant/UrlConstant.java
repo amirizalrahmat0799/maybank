468d97766f7a0cef5d4722a46c0d5da89731eaeb
Amirizal <amirizalrahmat@penril.net>
2024-11-07 11:22:23 +0800

    Remove unused code

--- src/main/java/my/com/mbb/constant/UrlConstant.java
+++ src/main/java/my/com/mbb/constant/UrlConstant.java
@@ -6,7 +6,6 @@ import lombok.experimental.UtilityClass;
 public class UrlConstant {
     public static final String API_V1 = "/v1";
     public static final String CUSTOMER_ACCOUNT_BASE_URL = "/cust-acct";
-    public static final String GET_CUSTOMERS = "/get";
     public static final String CURRENCY_BASE_URL = "/currency";
 
 }
